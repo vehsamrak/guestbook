197d48f91f955ace97b7ec59ca69d9278d7c5bdf
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-23 16:27:20 +0300

    AbstractRepository - родительский класс для репозиториев сущностей

---
+++ src/Framework/AbstractRepository.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Author: Vehsamrak
+ * Date Created: 21.01.16 0:15
+ */
+
+namespace Framework;
+
+abstract class AbstractRepository
+{
+
+    private $connection;
+
+    public function __construct(\mysqli $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function createTables()
+    {
+        $this->connection->query("
+            CREATE TABLE IF NOT EXISTS `entries` (
+                `id` INT(11) NOT NULL AUTO_INCREMENT,
+                `author` VARCHAR(64) NOT NULL,
+                `text` TEXT NOT NULL,
+                PRIMARY KEY (`id`)
+            )
+            COLLATE='utf8_general_ci'
+            ENGINE=InnoDB;
+        ");
+    }
+}
