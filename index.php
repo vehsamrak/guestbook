1d8dce892d1771a5d9ec2ed6b9001cd2e0f4966f
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2016-01-23 16:22:00 +0300

    Изменение константы ROOT_DIRECTORY в index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Date Created: 19.01.16 21:34
  */
 
-const BASE_DIRECTORY = __DIR__;
+const ROOT_DIRECTORY = __DIR__;
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -12,7 +12,7 @@ ini_set('display_errors', 1);
 spl_autoload_register(function ($className) {
     $className = str_replace("\\", "/", $className);
     $namespace = str_replace("\\", "/", __NAMESPACE__) ?: '/';
-    $class = BASE_DIRECTORY . "/src/" . $namespace . $className . ".php";
+    $class = ROOT_DIRECTORY . "/src/" . $namespace . $className . ".php";
 
     if (file_exists($class)) {
         include_once($class);
